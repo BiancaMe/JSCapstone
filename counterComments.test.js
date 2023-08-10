c7ad98d6980d0df1eaa99844058824a09ae25ea9
BiancaMe <bianquis0410@gmail.com>
2023-08-10 12:18:56 -0300

    Fix linters errors

--- counterComments.test.js
+++ counterComments.test.js
@@ -3,7 +3,6 @@
 */
 
 import { setCounter } from './src/modules/comments';
-import { getComments } from './src/modules/GETcomments';
 
 document.body.innerHTML = '<h3>Comments</h3><p class="cont">()</p>';
 
@@ -24,7 +23,6 @@ describe('Test Counter Comments', () => {
   });
 
   test('Counter comments Case 2', () => {
-
     comments = [{ comment: 'Hola', creation_date: '2023-08-10', username: 'Bianca' },
       { comment: 'testingggggg', creation_date: '2023-08-10', username: 'Melanie' },
       { comment: 'testinggg', creation_date: '2023-08-10', username: 'User' },
@@ -40,7 +38,7 @@ describe('Test Counter Comments', () => {
     expect(document.querySelector('.cont').textContent).toBe(res);
   });
 
-  test('Counter comments Case 3',  () => {
+  test('Counter comments Case 3', () => {
     comments = [];
     setCounter(comments);
     let res = '(11)';

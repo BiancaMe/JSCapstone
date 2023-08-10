fab1ad79f1340e4164e6b926fa5619c961b0fac4
Ronald Mjonono <mjay@M-JAY.local>
2023-08-10 23:15:54 +0200

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./css/index.css";
 import "./css/popUp.css";
 import { comments } from "./modules/comments";
+
 // Track likes and comments using objects
 const likeCounts = {};
 const commentCounts = {};
+
 async function fetchData(apiUrl) {
   try {
     const response = await fetch(apiUrl);
@@ -19,17 +21,21 @@ async function fetchData(apiUrl) {
     throw error;
   }
 }
+
 async function loadItems() {
   const baseApiUrl = "https://api.tvmaze.com/shows?page=1";
   const shows = await fetchData(baseApiUrl);
   const kanbanBoard = document.getElementById("kanbanBoard");
+
   for (let i = 0; i < shows.length; i += 3) {
     const row = document.createElement("div");
     row.classList.add("row");
+
     for (let j = i; j < i + 3 && j < shows.length; j += 1) {
       const show = shows[j];
       const likeCount = likeCounts[show.id] || 0; // Use the tracked like count
       const commentCount = commentCounts[show.id] || 0; // Use the tracked comment count
+
       const itemCard = document.createElement("div");
       itemCard.classList.add("item-card");
       itemCard.setAttribute("id", show.id);
@@ -44,19 +50,24 @@ async function loadItems() {
         <span class="comment-icon">💬</span>
         <span class="comment-count">${commentCount}</span>
       `;
+
       const likeButton = itemCard.querySelector(".like-icon");
       likeButton.addEventListener("click", () => {
         likeCounts[show.id] = (likeCounts[show.id] || 0) + 1;
         const likeCountElement = itemCard.querySelector(".like-count");
         likeCountElement.textContent = likeCounts[show.id];
       });
+
       const commentButton = itemCard.querySelector(".comments-button");
       initComment(commentButton, itemCard, show.id); // Pass itemCard to initComment
+
       row.appendChild(itemCard);
     }
+
     kanbanBoard.appendChild(row);
   }
 }
+
 function initComment(btn, card, id) {
   btn.addEventListener("click", async (e) => {
     e.preventDefault();
@@ -68,19 +79,5 @@ function initComment(btn, card, id) {
     commentCountElement.textContent = commentCounts[id];
   });
 }
+
 document.addEventListener("DOMContentLoaded", loadItems);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

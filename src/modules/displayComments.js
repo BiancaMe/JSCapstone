6f9131e14f29bcaac23bd5e49f97a757f72765b2
Ronald Mjonono <mjay@M-JAY.local>
2023-08-12 09:36:53 +0200

    implement required changes

--- src/modules/displayComments.js
+++ src/modules/displayComments.js
@@ -1,31 +1,34 @@
-// eslint-disable-next-line import/no-cycle
-import { cleanComments, setCounter } from './comments';
-import { getShowData, getComments } from './GETcomments';
+import { cleanComments, setCounter } from "./comments";
+import { getShowData, getComments } from "./GETcomments";
+import { updateCommentCounter } from "./counter"; // Import the updated setCounter function
 
 const displayComments = async (id) => {
   const pathById = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Ak1TTqB18F0chgbGj32L/comments/?item_id=${id}`;
   const comments = await getComments(pathById);
   const show = await getShowData(`https://api.tvmaze.com/shows/${id}`);
-  document.querySelector('#dataShow').innerHTML = ` <img src="${show.image.medium}"> 
-                                                    <h2 class="p-title">${show.name}</h2>
-                                                    <ul class="p-features">
-                                                        <li class="f-item"> <p> Genre:</p>${show.genres}</li>
-                                                        <li class="f-item"> <p>Premiered:</p>${show.premiered}</li>
-                                                        <li class="f-item"> <p>Language:</p> ${show.language}</li>
-                                                        <li class="f-item"> <p>Official Site:</p>${show.officialSite}</li>
-                                                    </ul>`;
-  const list = document.querySelector('.list-comments');
+  document.querySelector("#dataShow").innerHTML = `
+    <img src="${show.image.medium}">
+    <h2 class="p-title">${show.name}</h2>
+    <ul class="p-features">
+      <li class="f-item"><p>Genre:</p>${show.genres}</li>
+      <li class="f-item"><p>Premiered:</p>${show.premiered}</li>
+      <li class="f-item"><p>Language:</p> ${show.language}</li>
+      <li class="f-item"><p>Official Site:</p>${show.officialSite}</li>
+    </ul>`;
+
+  const list = document.querySelector(".list-comments");
   cleanComments();
-  setCounter(comments);
+  updateCommentCounter(comments); // Call the updated setCounter function
   if (comments.length !== undefined) {
     comments.forEach((elem) => {
-      const add = document.createElement('li');
-      add.setAttribute('class', 'item-comment');
-      add.innerHTML = ` <div class="c-user-data">
-                            <h4 class="c-user">${elem.username}</h4>
-                            <p class="c-date">${elem.creation_date}</p>
-                        </div>
-                        <p class="comment">${elem.comment}</p>`;
+      const add = document.createElement("li");
+      add.setAttribute("class", "item-comment");
+      add.innerHTML = `
+        <div class="c-user-data">
+          <h4 class="c-user">${elem.username}</h4>
+          <p class="c-date">${elem.creation_date}</p>
+        </div>
+        <p class="comment">${elem.comment}</p>`;
       list.appendChild(add);
     });
   }
